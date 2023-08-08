cec6476b15f21dc8eb5e3598b1d086c03285119d
stevengorlicki <stevengorlicki@uci.edu>
2023-08-07 20:13:39 -0700

    V 1.5 Added Page Buttons (Non-functional), Added Hover Color Change

--- website/static/theme_switch.js
+++ website/static/theme_switch.js
@@ -1,4 +1,4 @@
-document.getElementById('mode-switch').addEventListener('change', function(event) {
+document.getElementById('mode-switch').addEventListener('change', function (event) {
     let modeIcon = document.getElementById('mode-icon');
     let body = document.querySelector('body');
     let modeText = document.getElementById('mode-text');
@@ -24,14 +24,41 @@ document.getElementById('mode-switch').addEventListener('change', function(event
 });
 
 // Add an event listener to the dropdown to stop click propagation
-document.querySelector('.dropdown-menu').addEventListener('click', function(event) {
+document.querySelector('.dropdown-menu').addEventListener('click', function (event) {
     event.stopPropagation();
 });
 
 // Add an event listener to close the dropdown when clicking outside
-document.addEventListener('click', function(event) {
+document.addEventListener('click', function (event) {
     const settingsDropdown = document.getElementById('settingsDropdown');
     if (!settingsDropdown.contains(event.target)) {
         $(".dropdown-menu").removeClass("show");
     }
+});
+
+function showLoginForm() {
+    document.getElementById('loginModalBody').classList.remove('d-none');
+    document.getElementById('signupModalBody').classList.add('d-none');
+}
+
+document.getElementById('loginSignup').addEventListener('click', function () {
+    showLoginForm();
+    $('#loginSignupModal').modal('show');
+});
+
+document.getElementById('needToSignUpButton').addEventListener('click', function () {
+    // Hide the login modal body and show the sign-up modal body
+    document.getElementById('loginModalBody').classList.add('d-none');
+    document.getElementById('signupModalBody').classList.remove('d-none');
+});
+
+document.getElementById('signUpButton').addEventListener('click', function () {
+    // Implement your sign-up logic here...
+    // After successful sign-up, you can close the modal if needed:
+    $('#loginSignupModal').modal('hide');
+});
+
+// Reset the modal content to login form when the modal is closed
+$('#loginSignupModal').on('hidden.bs.modal', function () {
+    showLoginForm();
 });
