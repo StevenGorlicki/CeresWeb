ebb525945f9a67e29c3ef1255237928a29cd438e
StevenGorlicki <sgorlick@uci.edu>
2023-10-23 00:04:22 -0700

    Added student_forms page

--- website/urls.py
+++ website/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('student_forms/', views.student_forms, name='student_forms'),  # The new URL pattern
 ]
--- website/views.py
+++ website/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html', {})
+
+
+def student_forms(request):
+    return render(request, 'student_forms.html', {})
+
+
