d1044bdebe3fc2453415712e889d2f8c2af568a1
StevenGorlicki <sgorlick@uci.edu>
2023-12-23 18:04:46 -0800

    added collectstatic

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
+# Check if we are in a production environment
+if not settings.DEBUG:
+    # Run collectstatic programmatically
+    call_command('collectstatic', '--noinput', verbosity=0)
+
+# Continue with the normal execution of the WSGI application
 application = get_wsgi_application()
